bdb758ca7ee0ab6ac4bb55372b7682db42f711ed
CSXRP2009 <CSXRP@LAPTOP-ITVH3M10>
2023-02-03 22:07:10 -0500

    Added some comments to explain code.

--- src/main/java/com/mycompany/csc229_assignment01/CSC229_Assignment01.java
+++ src/main/java/com/mycompany/csc229_assignment01/CSC229_Assignment01.java
@@ -8,9 +8,10 @@ package com.mycompany.csc229_assignment01;
 public class CSC229_Assignment01 {
     
      public static void main(String[] args) {
+         // instanciating some objects
         Course CSC229 = new Course();
         Course CSC230 = new Course(1, "Computer Science", "230");
-        
+        // calling the methods or constuctors that are strings
         CSC229.setName ("Data Structures and Algorithms");
         System.out.println(CSC229.getName());
         System.out.println(CSC230.getName());
